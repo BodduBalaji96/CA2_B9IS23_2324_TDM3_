fb34c088632682be44e7521b32c6367c39287692
BodduBalaji96 <boddubalaji96@gmail.com>
2025-07-21 15:46:34 +0530

    updating Home.js

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -22,3 +22,30 @@ const Home = ({ onLogin }) => {
                 return null;
         }
     };
+return (
+        <div className="home-container">
+            <h1>Welcome to Balaji Khna Kajana  </h1>
+            <p>Savor authentic flavors with a royal touch – where tradition meets taste </p>
+            <div className="home-buttons">
+                <button onClick={() => setActiveComponent('register')}>Register</button>
+                <button onClick={() => setActiveComponent('login')}>Login</button>
+                <button onClick={() => setActiveComponent('viewMenu')}>Menu</button>
+                <button onClick={() => setActiveComponent('contactUs')}>Contact Us</button>
+            </div>
+
+            { 
+            <img 
+                className="restaurant-image" 
+                src="/a.jpg" 
+                alt="Restaurant" 
+            /> 
+            }
+
+            <div className="component-container">
+                {renderComponent()}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
